3ca7ccdaf79f64110204861632e16d51033e2f90
__RAJ__   |-_-| <raj.cncs.work@gmail.com>
2026-08-26 21:17:07 +0530

    Organize C practice files

---
+++ daily practice/first_c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main() {
+    char first_C_code_date[100] = "24th August 2026";
+    printf("first line of  code  of C\n");
+    printf("i wrote my first c code on %s",first_C_code_date);
+    return 0;
+}
--- first_c.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-
-    printf("first line of  code  of C");
-    return 0;
-}
